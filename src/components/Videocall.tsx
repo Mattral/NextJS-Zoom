4d978a894cd5eba83e99c7c9a490e61d2b645774
Min Htet Myet (Mattral) <88831350+Mattral@users.noreply.github.com>
2024-07-28 18:57:04 +0700

    Update Videocall.tsx

--- src/components/Videocall.tsx
+++ src/components/Videocall.tsx
@@ -1,5 +1,147 @@
 "use client";
 
+import { CSSProperties, useRef, useState, useEffect } from "react";
+import ZoomVideo, {
+  type VideoClient,
+  VideoQuality,
+  type VideoPlayer,
+} from "@zoom/videosdk";
+import { CameraButton, MicButton } from "./MuteButtons";
+import { WorkAroundForSafari } from "@/lib/utils";
+import { PhoneOff } from "lucide-react";
+import { Button } from "./ui/button";
+
+const Videocall = (props: { slug: string; JWT: string }) => {
+  const session = props.slug;
+  const jwt = props.JWT;
+  const [inSession, setInSession] = useState(false);
+  const client = useRef<typeof VideoClient>(ZoomVideo.createClient());
+  const [isVideoMuted, setIsVideoMuted] = useState(false);  // Default to false
+  const [isAudioMuted, setIsAudioMuted] = useState(false);  // Default to false
+  const videoContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    console.log("Video container ref:", videoContainerRef.current);
+  }, [videoContainerRef]);
+
+  const joinSession = async () => {
+    console.log("Joining session...");
+    await client.current.init("en-US", "Global", { patchJsMedia: true });
+    client.current.on(
+      "peer-video-state-change",
+      (payload) => void renderVideo(payload)
+    );
+    await client.current.join(session, jwt, userName).catch((e) => {
+      console.log("Join error:", e);
+    });
+    setInSession(true);
+    const mediaStream = client.current.getMediaStream();
+    // @ts-expect-error https://stackoverflow.com/questions/7944460/detect-safari-browser/42189492#42189492
+    window.safari
+      ? await WorkAroundForSafari(client.current)
+      : await mediaStream.startAudio();
+    setIsAudioMuted(false);  // Set to false to indicate the audio is on
+    await mediaStream.startVideo().catch((e) => {
+      console.log("Start video error:", e);
+    });
+    setIsVideoMuted(false);  // Set to false to indicate the video is on
+    await renderVideo({
+      action: "Start",
+      userId: client.current.getCurrentUserInfo().userId,
+    });
+  };
+
+  const renderVideo = async (event: {
+    action: "Start" | "Stop";
+    userId: number;
+  }) => {
+    const mediaStream = client.current.getMediaStream();
+    console.log("Rendering video:", event);
+    if (event.action === "Stop") {
+      const element = await mediaStream.detachVideo(event.userId);
+      Array.isArray(element)
+        ? element.forEach((el) => el.remove())
+        : element.remove();
+    } else {
+      const userVideo = await mediaStream.attachVideo(
+        event.userId,
+        VideoQuality.Video_360P
+      );
+      console.log("User video:", userVideo);
+      videoContainerRef.current!.appendChild(userVideo as VideoPlayer);
+    }
+  };
+
+  const leaveSession = async () => {
+    client.current.off(
+      "peer-video-state-change",
+      (payload: { action: "Start" | "Stop"; userId: number }) =>
+        void renderVideo(payload)
+    );
+    await client.current.leave().catch((e) => console.log("Leave error:", e));
+    // hard refresh to clear the state
+    window.location.href = "/";
+  };
+
+  return (
+    <div className="flex h-full w-full flex-1 flex-col">
+      <h1 className="text-center text-3xl font-bold mb-4 mt-0">
+        Session: {session}
+      </h1>
+      <div
+        className="flex w-full flex-1"
+        style={inSession ? {} : { display: "none" }}
+      >
+        {/* @ts-expect-error html component */}
+        <video-player-container ref={videoContainerRef} style={videoPlayerStyle} />
+      </div>
+      {!inSession ? (
+        <div className="mx-auto flex w-64 flex-col self-center">
+          <div className="w-4" />
+          <Button className="flex flex-1" onClick={joinSession} title="join session">
+            Join
+          </Button>
+        </div>
+      ) : (
+        <div className="flex w-full flex-col justify-around self-center">
+          <div className="mt-4 flex w-[30rem] flex-1 justify-around self-center rounded-md bg-white p-4">
+            <CameraButton
+              client={client}
+              isVideoMuted={isVideoMuted}
+              setIsVideoMuted={setIsVideoMuted}
+              renderVideo={renderVideo}
+            />
+            <MicButton
+              isAudioMuted={isAudioMuted}
+              client={client}
+              setIsAudioMuted={setIsAudioMuted}
+            />
+            <Button onClick={leaveSession} title="leave session">
+              <PhoneOff />
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Videocall;
+
+const videoPlayerStyle = {
+  height: "75vh",
+  marginTop: "1.5rem",
+  marginLeft: "3rem",
+  marginRight: "3rem",
+  alignContent: "center",
+  borderRadius: "10px",
+  overflow: "hidden",
+} as CSSProperties;
+
+const userName = `User-${new Date().getTime().toString().slice(8)}`;
+
+/*"use client";
+
 import { CSSProperties, useRef, useState } from "react";
 import ZoomVideo, {
   type VideoClient,
@@ -87,7 +229,7 @@ const Videocall = (props: { slug: string; JWT: string }) => {
         className="flex w-full flex-1"
         style={inSession ? {} : { display: "none" }}
       >
-        {/* @ts-expect-error html component */}
+        {/* @ts-expect-error html component }
         <video-player-container ref={videoContainerRef} style={videoPlayerStyle} />
       </div>
       {!inSession ? (
@@ -134,3 +276,4 @@ const videoPlayerStyle = {
 } as CSSProperties;
 
 const userName = `User-${new Date().getTime().toString().slice(8)}`;
+*/
